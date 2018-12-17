31188404dbcab17b6fc03f89ad50fe7b05b9de0b
Andy Ng <andy.hy.ng@gmail.com>
2018-12-17 00:15:21 -0500

    added project descriptions, in-progress nav bar work

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,3 @@
-$(document).ready(function () {
-    // setInterval(() => {$("h1").toggleClass(".flip-trigger")}, 800);
-})
+$('button').on('click', function () {
+    $('body').toggleClass('open');
+});
