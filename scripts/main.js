2168133e6acf7849b57643de769abdcc6367455e
Andy Ng <andy.hy.ng@gmail.com>
2018-12-18 11:13:59 -0500

    added ScrollReveal, organized JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,45 @@
-$('.header-nav-button').on('click', function () {
-    $('body').toggleClass('open');
-});
+const revealSection = () => {
+    ScrollReveal().reveal('#about', { reset: true, delay: 50 });
+    ScrollReveal().reveal("#projects", { reset: true, delay: 50 });
+    ScrollReveal().reveal(".project-container", { reset: true, delay: 50 });
+    ScrollReveal().reveal("#skills", { reset: true, delay: 50 });
+    ScrollReveal().reveal("#contact", { reset: true, delay: 50 });
+}
 
-$('.main-nav-anchors').on('click', function() {
-    $('body').toggleClass('open');
+const mainNavToggles = () => {
+    $('.header-nav-button').on('click', function () {
+        $('body').toggleClass('open');
+    });
+
+    $('.main-nav-anchors').on('click', function () {
+        $('body').toggleClass('open');
+    })
+}
+
+const init = () => {
+    revealSection();
+    mainNavToggles();
+}
+
+$(function() {
+    init();
 })
 
+// $(function() {
+//     if (isScrolledIntoView(document.getElementsByClassName('.project-container-img'))) {
+//         console.log('hi');
+//         $('.project-container-img img').toggleClass('hovered');
+//     }
+// })
+
+// function isScrolledIntoView(el) {
+//   var rect = el.getBoundingClientRect();
+//   var elemTop = rect.top;
+//   var elemBottom = rect.bottom;
+
+//   // Only completely visible elements return true:
+//   var isVisible = elemTop >= 0 && elemBottom <= window.innerHeight;
+//   // Partially visible elements return true:
+//   //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+//   return isVisible;
+// }
