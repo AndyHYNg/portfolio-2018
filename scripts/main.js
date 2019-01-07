f6f8de5656effaa6689708f12bec2809bd394914
Andy Ng <andy.hy.ng@gmail.com>
2019-01-07 16:31:27 -0500

    removed unnecessary comments, using css smooth-scroll for performance, fixed mobile touch events

--- scripts/main.js
+++ scripts/main.js
@@ -5,17 +5,25 @@ const revealSection = () => {
     ScrollReveal().reveal("#contact", { reset: true, delay: 50 });
 }
 
-const smoothScroll = () => {
-    $(".main-nav-anchors").smoothScroll({speed: 500});
-    $(".down-arrow").smoothScroll({ speed: 500 });
-}
+// const smoothScroll = () => {
+//     $(".main-nav-anchors").smoothScroll({speed: 500});
+//     $(".down-arrow").smoothScroll({ speed: 500 });
+// }
 
 const mainNavToggles = () => {
     $('.header-nav-button').on('click', function () {
         $('body').toggleClass('open');
     });
 
+    $('.header-nav-button').on('touch', function () {
+        $('body').toggleClass('open');
+    });
+
     $('.main-nav-anchors').on('click', function () {
+        $('body').toggleClass('open');
+    })
+
+    $('.main-nav-anchors').on('touch', function () {
         $('body').toggleClass('open');
     })
 }
@@ -23,7 +31,7 @@ const mainNavToggles = () => {
 const init = () => {
     revealSection();
     mainNavToggles();
-    smoothScroll();
+    // smoothScroll();
 }
 
 $(function() {
